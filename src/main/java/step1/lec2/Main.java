d82004d16d5db6e248e253329d7563800e932e63
shivam-0718 <shivam.vyas.1807@gmail.com>
2026-09-12 19:07:47 +0530

    Added Step 1 Lec 2 Main class

---
+++ src/main/java/step1/lec2/Main.java
@@ -0,0 +1,13 @@
+package step1.lec2;
+
+/**
+ * This is Striver's A2Z DSA Course Step 1.2 -> Patterns
+ * Here you can call pattern classes and test the running of the same
+ * by calling the pattern classes and invoking the same in main()
+ * present in this class.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
