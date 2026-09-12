a8e1c0a33c5d04fc00c20bc7e638e85d3cb8bc03
shivam-0718 <shivam.vyas.1807@gmail.com>
2026-09-12 19:22:06 +0530

    Added approach

--- src/main/java/step1/lec2/Main.java
+++ src/main/java/step1/lec2/Main.java
@@ -6,6 +6,19 @@ package step1.lec2;
  * by calling the pattern classes and invoking the same in main()
  * present in this class.
  */
+
+/**
+ * To solve patterns, there are 3 mandatory steps you need to do in order to solve any
+ * pattern in the world
+ * 1. For the outer loop, count the number of rows / lines
+ * 2. For the inner loop, count the number of columns and connect them somehow to the rows
+ *    This means, establish relationship between rows and columns
+ * 3. Print the content in the inner loop, and after moving out of the inner loop, move
+ *    to the next line by printing print()
+ *
+ * This rule is optional, and it is only applicable to specific pattern problems
+ * 1. Observe symmetry
+ */
 public class Main {
     public static void main(String[] args) {
 
