7cb4f0ee8580f9ecebecc6efa874a363e4198b73
shivam-0718 <shivam.vyas.1807@gmail.com>
2026-09-15 19:59:19 +0530

    Solved Step 1 Lec 2 Question 11

---
+++ src/main/java/step1/lec2/Pattern11.java
@@ -0,0 +1,37 @@
+package step1.lec2;
+
+public class Pattern11 {
+    // approach if starting from 0
+    public void pattern(int n){
+        int ans = 1;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                ans = 1;
+            } else {
+                ans = 0;
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(ans + " ");
+                ans = 1 - ans;
+            }
+            System.out.println();
+        }
+    }
+
+    // approach if starting from 1
+    public void pattern1(int n){
+        int ans = 1;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                ans = 0;
+            } else {
+                ans = 1;
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ans + " ");
+                ans = 1 - ans;
+            }
+            System.out.println();
+        }
+    }
+}
