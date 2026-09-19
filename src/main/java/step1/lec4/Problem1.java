b20340206eda504c0c4cf3d417901c053d4cd51f
shivam-0718 <shivam.vyas.1807@gmail.com>
2026-09-19 18:12:45 +0530

    Solved Step 1 Lec 4 Question 1

---
+++ src/main/java/step1/lec4/Problem1.java
@@ -0,0 +1,23 @@
+package step1.lec4;
+
+/**
+ * Write a program to count the digits of a given number
+ * Link: https://www.naukri.com/code360/problems/number-of-digits_9173
+ */
+public class Problem1 {
+    // brute force approach
+    public static int countDigits(int n) {
+        int count = 0;
+        while(n > 0) {
+            count++;
+            n = n/10;
+        }
+        return count;
+    }
+
+    // optimal approach
+    public static int countTheDigits(int n){
+        return (int)(Math.log10(n) + 1);
+    }
+
+}
