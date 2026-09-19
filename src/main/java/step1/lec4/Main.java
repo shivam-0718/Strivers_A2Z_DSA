29a15bf46861cff7dea2ea18a930f7fd39231fcf
shivam-0718 <shivam.vyas.1807@gmail.com>
2026-09-19 18:08:23 +0530

    Added Step 1 Lec 4 Main class

---
+++ src/main/java/step1/lec4/Main.java
@@ -0,0 +1,11 @@
+package step1.lec4;
+/**
+ * This is Striver's A2Z DSA Course Step 1.4 -> Basic Maths
+ * Here you can call the classes and test the running of the same
+ * by calling the specific method of the class and running the same in main()
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
