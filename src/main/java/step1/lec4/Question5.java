061e11f6d861b7c6a9768ef1d619f5211b3238da
shivam-0718 <shivam.vyas.1807@gmail.com>
2026-09-20 19:56:49 +0530

    Solved Step 1 Lec 4 Question 5

---
+++ src/main/java/step1/lec4/Question5.java
@@ -0,0 +1,21 @@
+package step1.lec4;
+
+/**
+ * Write a program to find whether a given number is Armstrong number or not
+ * Note: A number is said to be an Armstrong number <=> sum of the cubes of each
+ * digit is equal to the same number
+ *
+ * Link: https://www.naukri.com/code360/problems/check-armstrong_589
+ */
+public class Question5 {
+    public static boolean isArmstrong (int num) {
+        int n = num;
+        int sum = 0;
+        while (num > 0) {
+            int lastDigit = num % 10;
+            sum += (int)(Math.pow(lastDigit, 3));
+            num = num / 10;
+        }
+        return n == sum;
+    }
+}
