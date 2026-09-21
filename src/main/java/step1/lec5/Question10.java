32e04e7a89e8897db82dd6cfef3e41880f259dd6
shivam-0718 <shivam.vyas.1807@gmail.com>
2026-09-21 21:54:28 +0530

    Solved Step 1 Lec 5 Question 10

---
+++ src/main/java/step1/lec5/Question10.java
@@ -0,0 +1,29 @@
+package step1.lec5;
+
+/**
+ * Write a program to find the number of prime numbers till N
+ */
+public class Question10 {
+    public static int countPrimeNosTillN(int n) {
+        int count = 0;
+        for(int i = 1; i <= n; i++) {
+            if (isPrime(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static boolean isPrime(int number) {
+        int count = 0;
+        for (int i = 1; i <= (int)Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                count++;
+                if ((number / i) != i) {
+                    count++;
+                }
+            }
+        }
+        return count == 2;
+    }
+}
