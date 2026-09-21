26ef99888d8c10d03b6ea9d577d26588b8e5dd7f
shivam-0718 <shivam.vyas.1807@gmail.com>
2026-09-21 21:53:38 +0530

    Solved Step 1 Lec 5 (earlier 4) Question 9

---
+++ src/main/java/step1/lec5/Question9.java
@@ -0,0 +1,39 @@
+package step1.lec5;
+
+/**
+ * Write a program to check whether a given number is prime number or not
+ * Link: https://www.naukri.com/code360/problems/check-prime_624674
+ */
+public class Question9 {
+    // A number is said to be prime <=> it has 2 factors: 1 and number itself
+
+    // brute-force approach
+    public static boolean isPrime(int number) {
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+        return count == 2;
+    }
+
+    // time complexity is O(n), which is inefficient for higher number
+    // just like for finding the number of factors
+    // we are applying the loop until sqrt (Number)
+
+    // optimized approach
+    public static boolean isPrimeNumber(int number) {
+        int count = 0;
+        for (int i = 1; i <= (int)Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                count++;
+                if ((number / i) != i) {
+                    count++;
+                }
+            }
+        }
+        return count == 2;
+    }
+
+}
