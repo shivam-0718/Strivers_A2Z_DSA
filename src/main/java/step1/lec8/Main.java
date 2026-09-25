cc10f402bc09edb37ea2db274cdf839c258c6062
shivam-0718 <shivam.vyas.1807@gmail.com>
2026-09-25 20:49:24 +0530

    Added Step 1 Lec 8 Main class

---
+++ src/main/java/step1/lec8/Main.java
@@ -0,0 +1,8 @@
+package step1.lec8;
+/**
+ * This is Striver's A2Z DSA Course Step 1.8 (as per new UI) -> Basic Arrays
+ * Here you can call the classes and test the running of the same
+ * by calling the specific method of the class and running the same in main()
+ */
+public class Main {
+}
